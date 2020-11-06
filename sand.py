fb6d5ba481aee2a3051a5c46e23ebe15e29e975a
G M Bueno <guilherme.bueno@leafagriculture.com.br>
2020-11-05 23:02:29 -0300

    refactor

--- sand.py
+++ sand.py
@@ -33,48 +33,6 @@ This mask is a row and a col of air cells before and after the board that
 do not influence the logic (count of neighbors will be the same).
 '''
 
-def test_board():
-    '''
-    Just for testing. 1 is sand. 0 is air.
-    '''
-    h, w  = 10, 40
-    board = {}
-    for row in range(h+2):
-        for col in range(w+2):
-            board[row, col] = 0
-
-    ''' shapes/patterns '''
-
-    # (oscillator) blinker
-    board[3, 2] = 1
-    board[3, 3] = 1
-    board[3, 4] = 1
-
-    # (oscillator) toad
-    board[2,20] = 1
-    board[2,21] = 1
-    board[2,22] = 1
-    board[3,19] = 1
-    board[3,20] = 1
-    board[3,21] = 1
-
-    # (spaceship) glider
-    board[2,10] = 1
-    board[3,11] = 1
-    board[4,11] = 1
-    board[4,10] = 1
-    board[4,9] = 1
-
-    # (still life) bee-hive
-    board[7,5] = 1
-    board[7,6] = 1
-    board[8,4] = 1
-    board[8,7] = 1
-    board[9,5] = 1
-    board[9,6] = 1
-
-    return board
-
 def random_board():
     '''1 is sand. 0 is air. 2 '''
     board = {}
@@ -83,7 +41,7 @@ def random_board():
             board[row, col] = 0
     for row in range(1, h+1):
         for col in range(1, w+1):
-            board[row, col] = random.randint(0, 1)
+            board[row, col] = random.randint(0, 2)
     return board
 
 def hourglass_board():
@@ -151,19 +109,6 @@ def next_state(board):
                 count = -count
                 count = (count + 1)
 
-    # for row in range(h, 0, -1):
-    #     for col in range(w, 0, -1):
-    #         cell = new_board[row, col]
-    #         # if it's sand
-    #         if cell == 1:
-    #             # if row below is air, it was already dealt with, so we skip
-    #             if new_board[row+1, col] != 0 and mov_board[row, col] == False:
-    #                 if new_board[row+1, col+1] == 0:
-    #                     new_board[row+1, col+1] = 1
-    #                     new_board[row, col] = 0
-    #                 elif new_board[row+1, col-1] == 0:
-    #                     new_board[row+1, col-1] = 1
-    #                     new_board[row, col] = 0
     # print_board(new_board)
     return new_board
 
@@ -178,30 +123,7 @@ def print_board(board):
         print(row_cells)
         row_cells = []
 
-def count_neighbors(row, col, board):
-    # pretty simple to count. Once we have a mask, we don't need to test edges
-    count = 0
-    # up, right, down, left
-    if board[row-1, col] == 1: # up
-        count += 1
-    if board[row, col+1] == 1: # right
-        count += 1
-    if board[row+1, col] == 1: # down
-        count += 1
-    if board[row, col-1] == 1: # left
-        count += 1
-    # diagonals
-    if board[row-1, col+1] == 1: # up right
-        count += 1
-    if board[row+1, col+1] == 1: # down right
-        count += 1
-    if board[row+1, col-1] == 1: # down left
-        count += 1
-    if board[row-1, col-1] == 1: # up left
-        count += 1
-    return count
 
-# board = test_board()
 board = hourglass_board()
 while True:
     print('\n')
